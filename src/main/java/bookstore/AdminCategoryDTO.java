bffe52ea88e77c40adf6ea9f8c1371db3b8aa3f3
sdakf <sdakftraining@gmail.com>
2018-06-17 16:00:10 +0200

    selecting categories

--- src/main/java/bookstore/AdminCategoryDTO.java
+++ src/main/java/bookstore/AdminCategoryDTO.java
@@ -1,12 +1,17 @@
 package bookstore;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class AdminCategoryDTO
         implements CategoryInfoHolder {
     private String id;
     private String text;
+    private CategoryState state;
+    private AdminCategoryDTO parent;
+    private String parentCategoryId;
 }
--- src/main/java/bookstore/CategoryState.java
+++ src/main/java/bookstore/CategoryState.java
@@ -2,13 +2,15 @@ package bookstore;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CategoryState {
-    private Boolean open;
-    private Boolean selected;
-    private Boolean disabled;
+    private boolean open;
+    private boolean selected;
+    private boolean disabled;
 }
